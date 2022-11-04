f8810a541ea2f9d4cc24d5447513a6b4a76e143f
nikhil patkal <110008815+nikhilpatkal@users.noreply.github.com>
2022-11-04 19:08:29 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	int b=20;
+	int c;
+	
+	printf("%d/n",c);default value is 0
+	c=a;this is swaping technique
+	a=b;
+	b=c;
+	printf("%d\n%d",a,b);
+	return 0;
+}
